84ef09c7f1bc054cf2ae5b74862d9d39e6c1c032
panoramicthimble <cookingwgas@protonmail.com>
2022-07-30 11:25:27 -0300

    cleans commenting up

--- Day 11 - BlackJack/main.py
+++ Day 11 - BlackJack/main.py
@@ -41,12 +41,7 @@ def draw_card():
     card_values.remove(next_card)
     ''''''#print(card_values)#log
   return next_card
-
-# we could do this with getters... was it really worth it though?
-# def checkHands(hands):
-#   P1_hand = hands[0]
-#   PC_hand = hands[1]
-
+  
 
 def play_game(P1_hand, PC_hand):
 
@@ -216,62 +211,6 @@ P1_hand = []
 PC_hand = []
 
 play_game(P1_hand, PC_hand)
-  
-# P1_hand = []
-# PC_hand = []
-
-# P1_hand.append(draw_card())
-# PC_hand.append(draw_card())
-# P1_hand.append(draw_card())
-# PC_hand.append(draw_card())
-
-# player_total = sum(P1_hand)
-# opponent_total = sum(PC_hand)
-# print(f"Your cards: {P1_hand}, current score: {player_total}\nComputer's first card: {PC_hand[0]}")
-
-
-# if P1_hand[0] == P1_hand[1]:
-#   split = input(f"Two {P1_hand[0]}s, will you split them? Type 'y' for yes, 'n' for no: ").lower()
-# else:
-#   split = 'n'
-
-# accepted = False
-# while not accepted:
-#   if split == 'y' or split == 'n':
-#     accepted = True
-#   else:
-#     split = input("That wasn't an accepted response, type 'y' or 'n': ")
-
-# if split == 'y':
-#   card = P1_hand[0]
-#   split_hand = [card]
-#   #P1_hand.remove(card)
-#   P1_hand.append(draw_card())
-#   split_hand.append(draw_card())
-#   PC_hand = play_hand(P1_hand)
-#   play_split_hand(split_hand, PC_hand)
-
-# if split == 'n':
-#   play_hand(P1_hand)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 '''
 class Game:
@@ -295,87 +234,3 @@ def set_board(game):
     proceed = input("Would you like to play again?")
   
 '''
-
-
-
-
-  
-
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-
-
-
-
-
-# All this would save a lot of time if we were to extend this program to accomodate additional players.
-# deal_order = []
-# players = 2
-# for i in range (players*2):
-#   if i % players == 0:
-#     deal_order.append(PC_hand)
-#   elif i % (players-1) == 0:
-#     deal_order.append(P1_hand)
-# print(f"Deal order: {deal_order}")
-
-# ^^ The stipulation being that we would have to have the ability of passing by reference.
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
-
